49b6f332046d538a9459539d7bae34e96c6c00c1
LDprg <lukas_4dr@gmx.at>
2025-09-30 22:22:39 +0200

    add basic project with logging

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,71 @@
+use musicnow_shared::add;
+use regex::Regex;
+use scraper::{self, Html, Selector};
+use tauri::async_runtime::block_on;
+
+async fn get_client_id() -> String {
+    let resp = reqwest::get("https://soundcloud.com").await.unwrap();
+    let body = resp.text().await.unwrap();
+
+    let site = Html::parse_document(&body);
+    let script = Selector::parse("script[src]").unwrap();
+    let script_regex = Regex::new(r"https://.*/assets/0.*.js").unwrap();
+
+    let mut script_src = "";
+
+    for element in site.select(&script) {
+        if let Some(src) = element.value().attr("src")
+            && script_regex.is_match(src)
+        {
+            script_src = src;
+        }
+    }
+
+    println!("{}", script_src);
+
+    let resp = reqwest::get(script_src).await.unwrap();
+    let body = resp.text().await.unwrap();
+
+    let start = "\"client_id=";
+    let end = "\"";
+
+    let pos = body.find(start).unwrap();
+    let client_id = body.get(pos + start.len()..).unwrap();
+
+    let pos = client_id.find(end).unwrap();
+    let client_id = client_id.get(..pos).unwrap();
+
+    println!("{}", client_id);
+
+    client_id.to_string()
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let client_id = block_on(get_client_id());
+
+    format!(
+        "Hello, {}! You've been greeted from Rust! With client_id: \"{}\"",
+        name, client_id
+    )
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    console_error_panic_hook::set_once();
+
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .timezone_strategy(tauri_plugin_log::TimezoneStrategy::UseLocal)
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
+        .setup(|_| {
+            log::warn!("{}", add(1, 2));
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,5 @@
-use leptos::task::spawn_local;
-use leptos::{ev::SubmitEvent, prelude::*};
-use serde::{Deserialize, Serialize};
+use leptos::prelude::*;
+use musicnow_shared::add;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,59 +8,19 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
-#[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
-}
-
 #[component]
 pub fn App() -> impl IntoView {
-    let (name, set_name) = signal(String::new());
-    let (greet_msg, set_greet_msg) = signal(String::new());
+    let values = vec![0, 1, 2];
+    let test = add(1, 2);
 
-    let update_name = move |ev| {
-        let v = event_target_value(&ev);
-        set_name.set(v);
-    };
-
-    let greet = move |ev: SubmitEvent| {
-        ev.prevent_default();
-        spawn_local(async move {
-            let name = name.get_untracked();
-            if name.is_empty() {
-                return;
-            }
-
-            let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &name }).unwrap();
-            // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-            let new_msg = invoke("greet", args).await.as_string().unwrap();
-            set_greet_msg.set(new_msg);
-        });
-    };
+    log::error!("Test {}", test);
 
     view! {
         <main class="container">
-            <h1>"Welcome to Tauri + Leptos"</h1>
-
-            <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://docs.rs/leptos/" target="_blank">
-                    <img src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
-                </a>
-            </div>
-            <p>"Click on the Tauri and Leptos logos to learn more."</p>
-
-            <form class="row" on:submit=greet>
-                <input
-                    id="greet-input"
-                    placeholder="Enter a name..."
-                    on:input=update_name
-                />
-                <button type="submit">"Greet"</button>
-            </form>
-            <p>{ move || greet_msg.get() }</p>
+            {test} // this will just render "012"
+            <p>{values.clone()}</p> // or we can wrap them in <li>
+            <ul>{values.into_iter().map(|n| view! { <li>{n}</li> }).collect::<Vec<_>>()}</ul>
+            <button on:click=move |_| {log::error!("Test123");}>Debug</button>
         </main>
     }
 }
