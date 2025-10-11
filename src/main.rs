8f9d707498b63ab1b25eb14905c5ad0a1fe1891b
LDprg <lukas_4dr@gmx.at>
2025-10-11 14:11:44 +0200

    Remove desktop menu

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::thread;
 
-use dioxus::logger::tracing::Level;
+use dioxus::{logger::tracing::Level, prelude::desktop};
 
 mod app;
 mod components;
@@ -45,5 +45,7 @@ fn main() {
     #[cfg(not(feature = "server"))]
     dioxus::fullstack::set_server_url("http://127.0.0.1:8080");
 
-    dioxus::launch(App);
+    dioxus::LaunchBuilder::new()
+        .with_cfg(desktop!(dioxus::desktop::Config::new().with_menu(None)))
+        .launch(App);
 }
