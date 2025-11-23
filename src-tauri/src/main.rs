261206a8b5599fd10ff673b2a816a337c1de7be7
LDprg <lukas_4dr@gmx.at>
2025-11-23 22:11:32 +0100

    Add basic layout and temporary theme

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,20 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // fix linux wayland NVIDIA problems until fixed upstream
+    #[cfg(target_os = "linux")]
+    {
+        if std::path::Path::new("/dev/dri").exists()
+            && std::env::var("XDG_SESSION_TYPE").unwrap_or_default() == "wayland"
+        {
+            // Gnome Webkit is currently buggy under Wayland and KDE, so we will run it with XWayland mode.
+            // See: https://github.com/DioxusLabs/dioxus/issues/3667
+            unsafe {
+                // Disable explicit sync for NVIDIA drivers on Linux when using Way
+                std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+            }
+        }
+    }
+
     musicnow_lib::run()
 }
---
+++ src/routes/Search.svelte
@@ -0,0 +1,43 @@
+<script>
+</script>
+
+<form class="search-container">
+    <input type="text" placeholder="Search" />
+    <!-- svelte-ignore a11y_consider_explicit_label -->
+    <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+
+<style>
+    .search-container {
+        background: var(--theme-bg-dark);
+        display: flex;
+        position: sticky;
+        top: 0;
+        width: 100%;
+    }
+
+    .search-container input[type="text"] {
+        padding: 6px;
+        margin: 8px 0px 8px 8px;
+        font-size: 18px;
+        background: var(--theme-bg);
+        color: var(--theme-fg);
+        border: none;
+        flex: 1;
+    }
+
+    .search-container button {
+        float: right;
+        padding: 6px 10px;
+        margin: 8px;
+        background: var(--theme-bg);
+        color: var(--theme-prim);
+        font-size: 18px;
+        border: none;
+        cursor: pointer;
+    }
+
+    .search-container button:hover {
+        background: var(--theme-focus);
+    }
+</style>
